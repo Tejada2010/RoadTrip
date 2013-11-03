a6ba713a87595a81f709a59bbc2584397227b187
freddysalinas801 <freddysalinas801@gmail.com>
2013-11-03 16:19:32 -0600

    Added .jar file
    Jar file is for the Last.fm api

--- RoadTrip/RoadTrip/src/com/roadtrip/MainActivity.java
+++ RoadTrip/RoadTrip/src/com/roadtrip/MainActivity.java
@@ -1,8 +1,14 @@
 package com.roadtrip;
 
-import android.os.Bundle;
+import java.util.Collection;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
+import de.umass.lastfm.Artist;
+import de.umass.lastfm.Chart;
+import de.umass.lastfm.User;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lastFM();
     }
 
 
@@ -18,6 +25,20 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void lastFM() {
+    	 String key = "bda6ba1f6b4d727efd8b98377a5488c6"; //this is the key used in the Last.fm API examples
+    	    String user = "JRoar";
+    	    Chart<Artist> chart = User.getWeeklyArtistChart(user, 10, key);
+    	    DateFormat format = DateFormat.getDateInstance();
+    	    String from = format.format(chart.getFrom());
+    	    String to = format.format(chart.getTo());
+    	    System.out.printf("Charts for %s for the week from %s to %s:%n", user, from, to);
+    	    Collection<Artist> artists = chart.getEntries();
+    	    for (Artist artist : artists) {
+    	        System.out.println(artist.getName());
+    	    }
     }
     
 }
